5bb9cbefcad59c0f03303a2a263671bfd3485dc2
Jarno Le Conté <jarno@pti.nl>
2019-11-03 00:26:09 +0100

    Split logic from rendering

--- Sources/me.hckr.findertags.sdPlugin/color-wheel.jsx
+++ Sources/me.hckr.findertags.sdPlugin/color-wheel.jsx
@@ -8,72 +8,14 @@ const DEFAULT_TAGS = [
     { color: 'Gray', enabled: true },
 ];
 
-// Global variables
-var uuid,
-    actionInfo,
-    appInfo,
-    websocket,
-    colorWheelImage;
-
 // Preload data
-colorWheelImage = new Image();
+const colorWheelImage = new Image();
 colorWheelImage.ready = false;
 colorWheelImage.src = 'images/ColorWheelIcon@2x.png';
 colorWheelImage.onload = () => {
     colorWheelImage.ready = true;
 };
 
-// Startup
-function connectElgatoStreamDeckSocket(inPort, inUUID, inRegisterEvent, inInfo, inActionInfo) {
-    uuid = inUUID;
-    actionInfo = JSON.parse(inActionInfo);
-    appInfo = JSON.parse(inInfo);
-    websocket = new WebSocket('ws://127.0.0.1:' + inPort);
-
-    // if connection was established, the websocket sends
-    // an 'onopen' event, where we need to register our PI
-    websocket.onopen = function () {
-        // register property inspector to Stream Deck
-        websocket.send(JSON.stringify({
-            event: inRegisterEvent,
-            uuid: inUUID
-        }));
-
-        // Setup DOM / initialize React
-        initDOM({ settings: actionInfo.payload.settings });
-    };
-
-    websocket.onmessage = function (evt) {
-        // Received message from Stream Deck
-        var jsonObj = JSON.parse(evt.data);
-
-        console.log('onMessage', jsonObj)
-    };
-}
-
-// our method to pass values to the plugin
-function sendValueToPlugin (value, param) {
-    if (websocket && (websocket.readyState === 1)) {;
-        websocket.send(JSON.stringify({
-            'action': actionInfo['action'],
-            'event': 'sendToPlugin',
-            'context': uuid,
-            'payload': {
-                [param]: value
-            }
-        }));
-    }
-}
-
-function setSettings(settings) {
-    if (websocket && (websocket.readyState === 1)) {
-        websocket.send(JSON.stringify({
-            'event': 'setSettings',
-            'context': uuid,
-            'payload': settings,
-        }));
-    }
-}
 
 /* Helpers */
 
@@ -193,13 +135,13 @@ const PI = ({ settings, setSettings }) => {
     );
 };
 
-// Setup DOM with React
-function initDOM({ settings }) {
+// Startup and render
+function startup({ container, settings, setSettings }) {
     ReactDOM.render(
         <PI
             settings={settings}
             setSettings={setSettings}
         />,
-        document.querySelector('.sdpi-wrapper'),
+        container,
     );
 }
--- Sources/me.hckr.findertags.sdPlugin/custom-tag.jsx
+++ Sources/me.hckr.findertags.sdPlugin/custom-tag.jsx
@@ -1,64 +1,5 @@
 const COLORS = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Gray", "Custom"];
 
-// this is our global websocket, used to communicate from/to Stream Deck software
-// and some info about our plugin, as sent by Stream Deck software
-var uuid,
-    actionInfo,
-    appInfo,
-    websocket = null;
-
-function connectElgatoStreamDeckSocket(inPort, inUUID, inRegisterEvent, inInfo, inActionInfo) {
-    uuid = inUUID;
-    actionInfo = JSON.parse(inActionInfo);
-    appInfo = JSON.parse(inInfo);
-    websocket = new WebSocket('ws://127.0.0.1:' + inPort);
-
-    // if connection was established, the websocket sends
-    // an 'onopen' event, where we need to register our PI
-    websocket.onopen = function () {
-        // register property inspector to Stream Deck
-        websocket.send(JSON.stringify({
-            event: inRegisterEvent,
-            uuid: inUUID
-        }));
-
-        // Setup DOM / initialize React
-        initDOM({ settings: actionInfo.payload.settings });
-    };
-
-    websocket.onmessage = function (evt) {
-        // Received message from Stream Deck
-        var jsonObj = JSON.parse(evt.data);
-
-        console.log('onMessage', jsonObj)
-    };
-}
-
-// our method to pass values to the plugin
-function sendValueToPlugin (value, param) {
-    if (websocket && (websocket.readyState === 1)) {;
-        websocket.send(JSON.stringify({
-            'action': actionInfo['action'],
-            'event': 'sendToPlugin',
-            'context': uuid,
-            'payload': {
-                [param]: value
-            }
-        }));
-    }
-}
-
-function setSettings(settings) {
-    if (websocket && (websocket.readyState === 1)) {
-        websocket.send(JSON.stringify({
-            'event': 'setSettings',
-            'context': uuid,
-            'payload': settings,
-        }));
-    }
-}
-
-
 // React component for rendering the property inspector
 const PI = ({ settings, setSettings }) => {
     const [tag, setTag] = React.useState(settings.tag || "");
@@ -102,13 +43,13 @@ const PI = ({ settings, setSettings }) => {
     );
 };
 
-// Setup DOM with React
-function initDOM({ settings }) {
+// Startup and render
+function startup({ container, settings, setSettings }) {
     ReactDOM.render(
         <PI
             settings={settings}
             setSettings={setSettings}
         />,
-        document.querySelector('.sdpi-wrapper'),
+        container,
     );
 }
